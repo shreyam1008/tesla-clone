9a18a02c9e27b88151bbd5dea17ee002fd323897
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-09 05:31:57 +0545

    start

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* header */}
+      {/* items with snapping */}
+    </div>
+  );
+};
+
+export default App;
