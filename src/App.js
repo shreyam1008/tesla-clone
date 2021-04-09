9d252fd75b4d098b3543713a75c4054239f3fc61
Shreyam Adhikari <shreyam1008@gmail.com>
2021-04-09 22:59:12 +0545

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import React from "react";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App">
+      <Header />
+      
       {/* header */}
       {/* items with snapping */}
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import TeslaLogo from "../assets/teslaLogo.svg";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={TeslaLogo} alt="tesla logo" />
+      </div>
+
+      <div className="header__center">
+        <p>model S</p>
+        <p>model 3</p>
+        <p>model x</p>
+        <p>model y</p>
+        <p>model solar roof</p>
+        <p>solar panels</p>
+      </div>
+      
+      <div className="header__right">
+          <p>Shop</p>
+          <p>Tesla Account</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default Header;
